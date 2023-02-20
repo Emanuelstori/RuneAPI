e3754b3c06834656a27401d6acb837b05fba9ea6
Emanuel Stori de Lara <63805292+Emanuelstori@users.noreply.github.com>
2023-02-20 17:48:28 -0300

    new

--- src/main/java/com/rfapi/RFApi.java
+++ src/main/java/com/rfapi/RFApi.java
@@ -1,23 +1,11 @@
 package com.rfapi;
 
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
-
-public final class RFApi extends JavaPlugin {
-
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
-
+public final class RFApi {
+    public static void main(String[] args) {
+        System.out.println(new RFApi().test("world"));
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public String test(String name) {
+        return "Hello " + name;
     }
-
-    public void test(Player player) {
-        player.sendMessage("Working!");
-    }
-
 }
