b58a2a0b3cc11b9e0a4ed7a61f9335bf55fed5c7
Emanuel Stori de Lara <63805292+Emanuelstori@users.noreply.github.com>
2023-02-21 18:04:18 -0300

    last

--- src/main/java/com/rfapi/RFApiMain.java
+++ src/main/java/com/rfapi/RFApiMain.java
@@ -4,15 +4,23 @@ import org.bukkit.entity.Player;
 
 public final class RFApiMain {
     public static void main(String[] args) {
-        System.out.println(new RFApiMain().test("world"));
+        new RFApiMain().test(null);
+        new RFApiMain().testPlayer(null);
     }
 
-    public String test(String name) {
-        return "Hello " + name;
+    public String test(String test) {
+        if (test != null) {
+            return "Hello " + test;
+        }
+        return null;
     }
+
     public String testPlayer(Player player) {
-        String nick = player.getName();
-        player.sendMessage("A principio supimpa.");
-        return "Ok" + nick;
+        if (player != null) {
+            String nick = player.getName();
+            player.sendMessage("A principio supimpa.");
+            return "Ok" + nick;
+        }
+        return null;
     }
 }
